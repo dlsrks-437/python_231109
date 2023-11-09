9f3858ad502e0568f82679497941cd187dd2fbfc
dlsrks <paulhjkim3@gmail.com>
2023-11-09 15:33:07 +0900

    아이디 중복 시스템 완성 회원가입 시스템 완성

--- member_join_app.py
+++ member_join_app.py
@@ -15,6 +15,8 @@ class MainWindow(QMainWindow, form_class):
         self.setWindowTitle("회원가입프로그램")
 
         self.check_btn.clicked.connect(self.idCheck)
+        self.join_btn.clicked.connect(self.join)
+        self.clear_btn.clicked.connect(self.reset)
 
     def idCheck(self):
         memberid = self.memberid_edit.text()
@@ -31,9 +33,48 @@ class MainWindow(QMainWindow, form_class):
 
         if result == None:
             QMessageBox.warning(self, 'pass', '사용 가능')
+            self.check_btn.setStyleSheet("color:blue;")
         else:
             QMessageBox.warning(self, 'Fail', ' 사용 불가 \n아이디 중복')
+            self.check_btn.setStyleSheet("color:red;")
 
+        cur.close()
+        conn.commit()
+        conn.close()
+
+
+    def join(self):
+        # if check_btn.StyleSheet.color == 'black':
+        #     QMessageBox.warning(self, 'warning', '아이디 중복확인 필요')
+        memberid = self.memberid_edit.text()
+        memberpw = self.password_edit.text()
+        name = self.name_edit.text()
+        phone = self.phone_edit.text()
+        address = self.address_edit.text()
+        age = self.age_edit.text()
+
+        conn = pymysql.connect(host='localhost', user='root', password='12345', db='memberdb')
+
+        sql = f"INSERT INTO member VALUES('{memberid}', '{memberpw}', '{name}', '{phone}', '{address}', {age})"
+
+        cur = conn.cursor()  # 커서 생성
+        cur.execute(sql)  # SQL문 실행
+
+        cur.close()
+        conn.commit()
+        conn.close()
+
+        QMessageBox.warning(self, 'success', '가입 완료')
+        self.check_btn.setStyleSheet("color:black;")
+        self.reset()
+
+    def reset(self):
+        self.memberid_edit.clear()
+        self.password_edit.clear()
+        self.name_edit.clear()
+        self.phone_edit.clear()
+        self.address_edit.clear()
+        self.age_edit.clear()
 
 
 
